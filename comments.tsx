5cbfc863d8dff4a8d14dfdf1e1ab1ecb9695e300
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-11-12 20:07:17 +0200

    Updates

---
+++ comments.tsx
@@ -0,0 +1,111 @@
+  // <div data-cy="Todo" className="todo">
+  //   <label className="todo__status-label">
+  //     <input
+  //       data-cy="TodoStatus"
+  //       type="checkbox"
+  //       className="todo__status"
+  //     />
+  //   </label>
+
+  //   <span data-cy="TodoTitle" className="todo__title">CSS</span>
+
+  //   <button
+  //     type="button"
+  //     className="todo__remove"
+  //     data-cy="TodoDeleteButton"
+  //   >
+  //     ×
+  //   </button>
+
+  //   <div data-cy="TodoLoader" className="modal overlay">
+  //     <div className="modal-background has-background-white-ter" />
+  //     <div className="loader" />
+  //   </div>
+  // </div>
+
+  // <div data-cy="Todo" className="todo">
+  //   <label className="todo__status-label">
+  //     <input
+  //       data-cy="TodoStatus"
+  //       type="checkbox"
+  //       className="todo__status"
+  //     />
+  //   </label>
+
+  //   <form>
+  //     <input
+  //       data-cy="TodoTitleField"
+  //       type="text"
+  //       className="todo__title-field"
+  //       placeholder="Empty todo will be deleted"
+  //       defaultValue="JS"
+  //     />
+  //   </form>
+
+  //   <div data-cy="TodoLoader" className="modal overlay">
+  //     <div className="modal-background has-background-white-ter" />
+  //     <div className="loader" />
+  //   </div>
+  // </div>
+
+  // <div data-cy="Todo" className="todo">
+  //   <label className="todo__status-label">
+  //     <input
+  //       data-cy="TodoStatus"
+  //       type="checkbox"
+  //       className="todo__status"
+  //     />
+  //   </label>
+
+  //   <span data-cy="TodoTitle" className="todo__title">React</span>
+  //   <button
+  //     type="button"
+  //     className="todo__remove"
+  //     data-cy="TodoDeleteButton"
+  //   >
+  //     ×
+  //   </button>
+
+  //   <div data-cy="TodoLoader" className="modal overlay">
+  //     <div className="modal-background has-background-white-ter" />
+  //     <div className="loader" />
+  //   </div>
+  // </div>
+
+  // <div data-cy="Todo" className="todo">
+  //   <label className="todo__status-label">
+  //     <input
+  //       data-cy="TodoStatus"
+  //       type="checkbox"
+  //       className="todo__status"
+  //     />
+  //   </label>
+
+  //   <span data-cy="TodoTitle" className="todo__title">Redux</span>
+  //   <button
+  //     type="button"
+  //     className="todo__remove"
+  //     data-cy="TodoDeleteButton"
+  //   >
+  //     ×
+  //   </button>
+
+  //   <div data-cy="TodoLoader" className="modal overlay is-active">
+  //     <div className="modal-background has-background-white-ter" />
+  //     <div className="loader" />
+  //   </div>
+  // </div>
+// ``
+// <!-- Unable to add a todo
+// <br />
+// Unable to delete a todo
+// <br />
+// Unable to update a todo -->
+
+
+      // setselectedTodoId(currentTodo => (
+      //   currentTodo.filter(currentTodoId => {
+      //     console.log(currentTodoId, todoId);
+
+      //     return currentTodoId !== todoId;
+      //   })));
